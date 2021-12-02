9a026342bb41bbf16a793ff4bc524cfb0a866aba
Subhajit Sahu <wolfram77@gmail.com>
2021-12-02 13:32:05 +0530

    :turtle: update src

--- main.cu
+++ main.cu
@@ -1,11 +1,22 @@
-#include <cuda_runtime.h>
-#include <device_launch_parameters.h>
-#include <stdio.h>
-#include <string.h>
-#include "support.h"
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include "src/main.hxx"
+
+using std::vector;
+using std::max;
 
 
-const int threads = 4;
+
+
+const int THREADS = 4;
+
+
+// 1² + 2² + 3² + ...
+inline int sumSquares(int x) {
+  return x * (x + 1) * (2 * x + 1) / 6;
+}
 
 
 // Each thread computes pairwise product of multiple components of vector.
@@ -14,14 +25,14 @@ const int threads = 4;
 // of the total number of threads. This is done as long as it does not
 // exceed the length of the vector. Each thread maintains the sum of the
 // pairwise products it calculates.
-// 
+//
 // Once pairiwise product calculation completes, the per-thread sum is
 // stored in a cache, and then all threads in a block sync up to calculate
 // the sum for the entire block in a binary tree fashion (in log N steps).
 // The overall sum of each block is then stored in an array, which holds
 // this partial sum. This partial sum is completed on the CPU. Hence, our
 // dot product is complete.
-// 
+//
 // 1. Compute sum of pairwise product at respective index, while within bounds.
 // 2. Shift to the next component, by a stride of total no. of threads (4).
 // 3. Store per-thread sum in shared cache (for further reduction).
@@ -29,10 +40,10 @@ const int threads = 4;
 // 5. Reduce the sum in the cache to a single value in binary tree fashion.
 // 6. Store this per-block sum into a partial sum array.
 __global__ void kernel(float *c, float *a, float *b, int N) {
-  __shared__ float cache[threads];
+  __shared__ float cache[THREADS];
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int t = threadIdx.x, sum = 0;
-  
+
   while (i < N) {                // 1
     sum += a[i] * b[i];          // 1
     i += blockDim.x * gridDim.x; // 2
@@ -61,37 +72,36 @@ __global__ void kernel(float *c, float *a, float *b, int N) {
 // 8. Validate if the dot product is correct (on CPU).
 int main() {
   int N = 10;                     // 1
+  vector<float> a(N), b(N);       // 1
   size_t NB = N * sizeof(float);  // 1
-  float *a = (float*) malloc(NB); // 1
-  float *b = (float*) malloc(NB); // 1
-  for (int i=0; i<N; i++) { // 2
-    a[i] = (float) 2*i;     // 2
-    b[i] = (float) i;       // 2
-  }                         // 2
+  for (int i=0; i<N; i++) {  // 2
+    a[i] = 2*i;              // 2
+    b[i] = i;                // 2
+  }                          // 2
 
-  int threads = 2;                             // 3
-  int blocks = MAX(CEILDIV(N, threads), 2);    // 3
-  size_t NC = blocks * sizeof(float);          // 3
-  float* cpartial = (float*) malloc(NC);       // 3
+  int threads = 2;                           // 3
+  int blocks = max(ceilDiv(N, threads), 2);  // 3
+  vector<float> cpartial(blocks);            // 3
+  size_t NC = blocks * sizeof(float);        // 3
 
   float *aD, *bD, *cpartialD;        // 4
   TRY( cudaMalloc(&aD, NB) );        // 4
   TRY( cudaMalloc(&bD, NB) );        // 4
   TRY( cudaMalloc(&cpartialD, NC) ); // 4
-  TRY( cudaMemcpy(aD, a, NB, cudaMemcpyHostToDevice) ); // 4
-  TRY( cudaMemcpy(bD, b, NB, cudaMemcpyHostToDevice) ); // 4
+  TRY( cudaMemcpy(aD, a.data(), NB, cudaMemcpyHostToDevice) ); // 4
+  TRY( cudaMemcpy(bD, b.data(), NB, cudaMemcpyHostToDevice) ); // 4
 
   kernel<<<blocks, threads>>>(cpartialD, aD, bD, N); // 5
 
   TRY( cudaMemcpy(cpartial, cpartialD, NC, cudaMemcpyDeviceToHost) ); // 6
-  float c = SUM_ARRAY(cpartial, blocks); // 7
+  float c = sum(cpartial);  // 7
 
-  printf("a = "); PRINTVEC(a, N); printf("\n");
-  printf("b = "); PRINTVEC(b, N); printf("\n");
+  printf("a = "); println(a);
+  printf("b = "); println(b);
   printf("a .* b = %.1f\n", c);
 
-  float cexpected = (float) 2 * SUM_SQUARES(N-1); // 8
-  if (c != cexpected) {                           // 8
+  float cexpected = 2 * sumSquares(N-1);  // 8
+  if (c != cexpected) {                   // 8
     fprintf(stderr, "ERROR: a .* b != %.1f\n", cexpected);
   }
   return 0;
